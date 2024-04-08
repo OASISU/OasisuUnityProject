81c0c5e19a0a2eea02069e0fb179917db838f575
sujeong.kim <84218461+kimsujeong4811@users.noreply.github.com>
2024-04-08 17:00:34 +0900

    unity

---
+++ Assets/Ardity/Scripts/PulseMsgListener.cs
@@ -0,0 +1,139 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PulseMsgListener : MonoBehaviour
+{
+
+    private bool isCollectingData = false;
+    private List<int> heartRates = new List<int>();
+
+    private List<Animator> pulseFlowerAnimators = new List<Animator>();
+
+
+    void Start()
+    {
+       
+
+        // "PulseFlower" 오브젝트들의 Animator를 별도로 저장
+        for (int i = 1; i <= 5; i++)
+        {
+            GameObject pulseFlower = GameObject.Find("PulseFlower (" + i + ")");
+            if (pulseFlower != null)
+            {
+                Animator animator = pulseFlower.GetComponent<Animator>();
+                if (animator != null)
+                {
+                    pulseFlowerAnimators.Add(animator);
+                }
+            }
+            SetObjectAndChildrenVisibility("PulseFlower (" + i + ")", false); // 오브젝트를 보이지 않게 설정
+        }
+
+    }
+
+
+    public void OnMessageArrived(string msg)
+    {
+
+        try
+        {
+            int heartRate = int.Parse(msg);
+
+            if (isCollectingData)
+            {
+                heartRates.Add(heartRate);
+            }
+            else if (heartRate <= 10)
+            {
+                // 애니메이션 처음으로 되돌려놓기
+                foreach (Animator animator in pulseFlowerAnimators)
+                {
+                    if (animator != null)
+                    {
+                        animator.SetBool("isPulsed", false);
+                    }
+                }
+                // 모든 PulseFlower 오브젝트 숨기기
+                for (int i = 1; i<=5; i++)
+                {
+                    SetObjectAndChildrenVisibility("PulseFlower (" + i + ")", false); // 오브젝트를 보이지 않게 설정
+                }
+                
+                Debug.Log("평균내기 시작");
+                Debug.Log("Heart Rate: " + heartRate);
+                isCollectingData = true;
+                heartRates.Clear();
+                heartRates.Add(heartRate);
+                StartCoroutine(CollectDataFor5Seconds());
+            }
+        }
+        catch (System.Exception)
+        {
+            throw;
+        }
+    }
+
+    IEnumerator CollectDataFor5Seconds()
+    {
+        yield return new WaitForSeconds(5);
+
+        if (heartRates.Count > 0)
+        {
+            float average = 0;
+            foreach (int rate in heartRates)
+            {
+                average += rate;
+            }
+            average /= heartRates.Count;
+
+            Debug.Log("Average Heart Rate: " + average);
+
+            // 오브젝트 활성화 조건에 따라 처리
+            ShowObjectBasedOnAverage(average);
+        }
+
+        isCollectingData = false;
+    }
+
+    void ShowObjectBasedOnAverage(float average)
+    {
+        int index = -1;
+        if (average < 320) index = 0;
+        else if (average >= 320 && average < 340) index = 1;
+        else if (average >= 340 && average < 360) index = 2;
+        else if (average >= 360 && average < 380) index = 3;
+        else if (average >= 380) index = 4;
+
+        if (index != -1 && index < pulseFlowerAnimators.Count)
+        {
+            pulseFlowerAnimators[index].SetBool("isPulsed", true);
+            SetObjectAndChildrenVisibility("PulseFlower (" + (index + 1) + ")", true);
+            
+        }
+    }
+
+
+    public void SetObjectAndChildrenVisibility(string objectName, bool visible)
+    {
+        GameObject obj = GameObject.Find(objectName);
+        if (obj != null)
+        {
+            Debug.Log("Found object: " + objectName);
+            Renderer[] renderers = obj.GetComponentsInChildren<Renderer>();
+            foreach (Renderer renderer in renderers)
+            {
+                renderer.enabled = visible;
+                Debug.Log("Setting visibility for: " + renderer.gameObject.name + " to " + visible);
+                Debug.Log(renderer.gameObject.name + "의 enabled" + renderer.enabled);
+            }
+
+
+
+        }
+        else
+        {
+            Debug.Log("Could not find object: " + objectName);
+        }
+    }
+}
